3afff88af93be22f278fd3db63ded04266f69954
Remus Koos <remuskoos@yahoo.com>
2016-11-14 00:05:18 -0800

    Implemented recursing into subfolders.

--- script.service.panodpf.client/panodpf_client.py
+++ script.service.panodpf.client/panodpf_client.py
@@ -5,6 +5,7 @@ import json
 import time
 import socket
 import struct
+import random
 import argparse
 
 plugin_mode = True
@@ -24,23 +25,73 @@ except ImportError:
     def log(message, level=None):
         print message
 
-PANO_SUFFIXES = (".jpg", ".png", ".tiff", ".gif")
+ALLOWED_EXTENSIONS = (".jpg", ".png", ".tiff", ".gif")
 MAX_REQUEST_ID = 100000
 
 
-# FIXME: Add a randomize option.
-# FIXME: Recurse into subfolders.
-def pano_paths(pano_folder, recurse_into_subfolders=True):
+def get_random_file_path(pano_folder, recurse_into_subfolders=True):
+    log("get_random_file_path({0}, {1})".format(pano_folder, recurse_into_subfolders))
+    folder_list, file_list = xbmcvfs.listdir(xbmc.translatePath(pano_folder))
+
+    # Filter our file list by the allowed extensions.
+    file_list = [f for f in file_list if f.lower().endswith(ALLOWED_EXTENSIONS)]
+    folder_list_len = len(folder_list)
+    file_list_len = len(file_list)
+    total_len = folder_list_len + file_list_len
+
+    log("folder_list = {0}  file_list = {1}  folder_list_len = {2}  file_list_len = {3}".format(folder_list, file_list, folder_list_len, file_list_len))
+    if total_len == 0:
+        yield None
+
+    if not recurse_into_subfolders:
+        yield None if not file_list else os.path.join(pano_folder, random.choice(file_list))
+
+    # At this point 'total_len' is at least 1 so we have at least a file or a folder.
+    choice_idx = random.choice(xrange(total_len))
+    log("choice_idx = {0}".format(choice_idx))
+
+    # If this is the case it means that we have at least one file in the file list.
+    if choice_idx >= folder_list_len:
+        final_path = os.path.join(pano_folder, file_list[choice_idx - folder_list_len])
+        log("Returning path: '{0}' ...".format(final_path))
+        yield final_path
+
+    # FIXME: Can this cause an infinite recursion ?
+    get_random_file_path(os.path.join(pano_folder, folder_list[choice_idx]), recurse_into_subfolders)
+
+
+def xbmcvfs_walk(pano_folder, recurse_into_subfolders=True):
+    pending_folders = []
+    pending_folders.append(pano_folder)
+    while True:
+        try:
+            current_folder = pending_folders.pop()
+            folder_list, file_list = xbmcvfs.listdir(xbmc.translatePath(current_folder))
+            yield current_folder, folder_list, file_list
+
+            if not recurse_into_subfolders:
+                break
+
+            # Add the full paths of folders from folder list to the pending folders so we can traverse them at a later time.
+            pending_folders.extend([os.path.join(current_folder, f) for f in folder_list])
+        except IndexError:
+            break
+
+
+def pano_paths(pano_folder, recurse_into_subfolders=True, randomize=True):
+    log("pano_paths({0}, {1}, {2})".format(pano_folder, recurse_into_subfolders, randomize))
     if not pano_folder:
         return
 
-    folder_list, file_list = xbmcvfs.listdir(xbmc.translatePath(pano_folder))
-
-    folder = pano_folder
-    for file in file_list:
-        if file.lower().endswith(PANO_SUFFIXES):
-            # For each picture file we construct and yield the full path.
-            yield os.path.join(folder, file)
+    if randomize:
+        for path in get_random_file_path(pano_folder, recurse_into_subfolders):
+            yield path
+    else:
+        for folder, folder_list, file_list in xbmcvfs_walk(pano_folder, recurse_into_subfolders):
+            for file in file_list:
+                if file.lower().endswith(ALLOWED_EXTENSIONS):
+                    # For each picture file we construct and yield the full path.
+                    yield os.path.join(folder, file)
 
 
 def received_all_replies(sock, nreplies_expected):
@@ -130,11 +181,17 @@ def start_panodpf_client():
     while not monitor.abortRequested():
         pano_folder = __addon__.getSetting('dpf_folder')
         recurse = True if __addon__.getSetting('recurse_into_subfolders').lower() == "true" else False
+        randomize = True if __addon__.getSetting('randomize').lower() == "true" else False
 
-        for pano_path in pano_paths(pano_folder, recurse_into_subfolders=recurse):
+        for pano_path in pano_paths(pano_folder, recurse_into_subfolders=recurse, randomize=randomize):
+            if not pano_path:
+                log("Got None path from 'pano_paths({0}, {1}, {2})' ...".format(pano_folder, recurse, randomize))
+                # Sleep while the image is being displayed.
+                time.sleep(int(__addon__.getSetting('slideshow_delay')))
+                continue
+
             total_displays = int(__addon__.getSetting('total_displays')) + 1
             rotation = int(__addon__.getSetting('rotation'))
-            log("total_displays = {0}  rotation = {1}".format(total_displays, rotation))
             display_pano_params = {"path": pano_path, "rotation": rotation, "total_displays": total_displays}
             send_request_and_process_replies(sock, multicast_group, total_displays, "display_pano", display_pano_params, request_id)
             request_id = 0 if request_id >= MAX_REQUEST_ID else request_id + 1
@@ -143,8 +200,10 @@ def start_panodpf_client():
             time.sleep(int(__addon__.getSetting('slideshow_delay')))
 
 if plugin_mode:
+    log("Entering PanoDPFClient plugin mode ...")
     start_panodpf_client()
 else:
+    log("Entering PanoDPFClient standalone mode ...")
     parser = argparse.ArgumentParser(description='Filter panoramas by form factor.', epilog="At least one of '-l' or '-g' should be specified.")
     parser.add_argument("multicast_address", type=str, help="Multicast address to send the request to.")
     parser.add_argument("multicast_port", type=int, help="Multicast port to send the request to.")
