a90214494f6ef906b3e9965406876ec66f715b4e
Remus Koos <remuskoos@yahoo.com>
2016-11-14 00:06:21 -0800

    Implemented picture resize by a given height.

--- match_pics_by_format_factor.py
+++ match_pics_by_format_factor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import time
 import shutil
 import argparse
 
@@ -20,39 +21,51 @@ def matches_ff_rules(ff, le, ge):
     return False
 
 
-def copy_file(full_file_path, target_folder, destination_folder):
-    final_folder = os.path.join(target_folder, destination_folder)
-    if not os.path.exists(final_folder):
-        os.makedirs(final_folder)
-
+def copy_file(full_file_path, final_folder, w, h):
     try:
-        print "  Copying file '{0}' to folder '{1}' ...".format(full_file_path, final_folder)
+        print "    Copying image '{0}' with size ({1}, {2}) to folder '{3}' ...".format(full_file_path, w, h, final_folder)
         shutil.copy(full_file_path, final_folder)
     except EnvironmentError as e:
-        print "Could not copy file '{0}' to folder '{1}'".format(full_file_path, final_folder)
+        print "    Could not copy image '{0}' to folder '{1}': {2}".format(full_file_path, final_folder, e)
 
 
-def match_pics_by_form_factor(folder, recursive, le, ge, copy_to_folder):
+def match_pics_by_form_factor(folder, recursive, le, ge, copy_to_folder, resize_to_height):
     total_pictures = 0
     matched_pictures = 0
+    start = time.time()
     for current_folder, folder_list, file_list in os.walk(folder):
         for f in file_list:
             # Check that it's a picture.
             if f.lower().endswith(PICTURE_EXTENSIONS):
                 total_pictures += 1
-                ipath = os.path.join(current_folder, f)
-                w, h = Image.open(ipath).size
+                image_path = os.path.join(current_folder, f)
+                image_object = Image.open(image_path)
+                w, h = image_object.size
                 ff = float(w)/h
                 if matches_ff_rules(ff, le, ge):
-                    print "'{0}': {1:.3}".format(ipath, ff)
+                    print "'{0}': {1:.3}".format(image_path, ff)
                     matched_pictures += 1
                     if copy_to_folder:
-                        copy_file(ipath, copy_to_folder, current_folder[len(folder):])
+                        final_folder = os.path.join(copy_to_folder, current_folder[len(folder):])
+                        # Create target folders if they don't exist.
+                        if not os.path.exists(final_folder):
+                            os.makedirs(final_folder)
+
+                        # If the current height of the image is less than the resize height just copy the image file.
+                        if h <= resize_to_height:
+                            copy_file(image_path, final_folder, w, h)
+                        else:
+                            resize_to_width = int(float(w * resize_to_height)/h)
+                            print "    Resizing image from ({0}, {1}) to ({2}, {3}) ...".format(w, h, resize_to_width, resize_to_height)
+                            resized_image_object = image_object.resize((resize_to_width, resize_to_height), resample=Image.LANCZOS)
+                            final_image_path = os.path.join(final_folder, os.path.basename(image_path))
+                            print "    Saving resized image to path '{0}' ...".format(final_image_path)
+                            resized_image_object.save(final_image_path)
 
         if not recursive:
             break
 
-    return total_pictures, matched_pictures
+    return total_pictures, matched_pictures, int(time.time() - start)
 
 
 def main():
@@ -62,13 +75,14 @@ def main():
     parser.add_argument("-l", "--less-than-or-equal", "--le", type=float, default=0, help="Form factor should be less than or equal to this valuse.")
     parser.add_argument("-g", "--greater-than-or-equal", "--ge", type=float, default=0, help="Form factor should be more than or equal to this valuse.")
     parser.add_argument("-c", "--copy-to-folder", type=str, help="Copy matched pictures to the specified folder maintaining the original folder structure.")
+    parser.add_argument("-e", "--resize-to-height", type=int, help="Resize matched pictures to the specified height if current height is bigger than the specified height.")
     args = parser.parse_args()
 
     if args.less_than_or_equal == 0 and args.greater_than_or_equal == 0:
         parser.print_help()
         return 1
 
-    total, matched = match_pics_by_form_factor(args.folder, args.recursive, args.less_than_or_equal, args.greater_than_or_equal, args.copy_to_folder)
-    print "Total pictures: {0}  {1} pictures: {2}".format(total, "Copied" if args.copy_to_folder else "Matched", matched)
+    total, matched, total_time = match_pics_by_form_factor(args.folder, args.recursive, args.less_than_or_equal, args.greater_than_or_equal, args.copy_to_folder, args.resize_to_height)
+    print "Total pictures: {0}  {1} pictures: {2}  Total time: {3} s".format(total, "Copied" if args.copy_to_folder else "Matched", matched, total_time)
 
 main()
